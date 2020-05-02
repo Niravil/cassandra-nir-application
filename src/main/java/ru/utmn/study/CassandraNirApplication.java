03af9113af5afd76abeaca1424abea6fceeade0b
Niravil <messerignacius@gmail.com>
2020-05-02 11:50:23 +0500

    * project initialisation

---
+++ src/main/java/ru/utmn/study/CassandraNirApplication.java
@@ -0,0 +1,13 @@
+package ru.utmn.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraNirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraNirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/utmn/study/CassandraNirApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.utmn.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassandraNirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
